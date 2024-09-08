80fcede9010939f91c4efbe0d1b8d44b85a97b9c
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-09-08 17:41:29 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -9,3 +9,16 @@ for (let i = 0, i < sales.length, i++) {
   
 return totalSales / sales.length
 }
+
+// Task 2: Determine Performance Rating
+function determinePerformanceRating (averageSales) {
+    if (averageSales > 10000)
+        return "Excellent"
+    if (averageSales >= 7000)
+        return "Good"
+    if (averageSales >= 4000)
+        return "Satisfactory"
+    else
+        return "Needs Improvement"
+}
+
