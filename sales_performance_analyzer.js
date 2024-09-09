4ff978ed9c97e3d971180fc400e79a84a25a1321
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-09-08 21:57:22 -0400

    Implement generatePerformanceReport function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -38,3 +38,33 @@ function findTopAndBottomPerformers(salespeople) {
     return { topPerformer, bottomPerformer }
   }
   
+// Task 4: Generate a Performance Report
+
+function generatePerformanceReport(salesData) {
+    let performanceReport = []
+    
+    for (let i = 0; i < salesData.length; i++) {
+      let averageSales = calculateAverageSales(salesData[i].sales) 
+      let performanceRating = determinePerformanceRating(averageSales) 
+      
+      performanceReport.push({
+        name: salesData[i].name
+        averageSales: averageSales
+        performanceRating: performanceRating
+      })
+    }
+    
+    let topAndBottomPerformers = findTopAndBottomPerformers(
+      performanceReport.map(function(person) {
+        return { name: person.name, sales: person.averageSales }
+      })
+    )
+    
+    return {
+      performanceDetails: performanceReport
+      topPerformer: topAndBottomPerformers.topPerformer.name
+      bottomPerformer: topAndBottomPerformers.bottomPerformer.name
+    }
+  }
+  
+
